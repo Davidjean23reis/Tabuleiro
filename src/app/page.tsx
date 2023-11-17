6d5a1f006bb10befe94dae51dd4cefea8a40e5aa
David <david.jean.reis23@gmail.com>
2023-11-17 13:53:27 -0300

    Criei  um Tabuleiro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Linha from '../../components/Linha'
-import styles from './styles/Linha.module.css'
 
+import styles from './styles/Linha.module.css'
+import Tabuleiro from '../../components/Tabuleiro'
 export default function () {
   return (
     <div className={styles.container}>
-      <Linha />
+     <Tabuleiro />
     </div>
   )
 }
